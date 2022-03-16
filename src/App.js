618e07316b3072a1476bdaf662c8229006eb7453
Platoni <platoni.injgia@flatrocktech.com>
2022-03-16 13:36:21 +0400

    React installation

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" exact element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </div>
+
+  );
+}
+
+export default App;
+
+
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomeScreen() {
+
+  return (
+      <React.Fragment>
+        <h1>Blog ...</h1>
+      </React.Fragment>
+  );
+}
+
+export default HomeScreen;

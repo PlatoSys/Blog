14ddc2a86d79a06b839b82970df29e837c41a705
PlatoSys <platoniinjgia@gmail.com>
2022-05-11 12:10:35 +0400

    Remove Header

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="app">
       <Router basename={process.env.PUBLIC_URL}>
-        <Header />
+        {/* <Header /> */}
         <main className="py-3">
           <Container>
             <Routes>
